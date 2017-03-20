433830726caecdeeecb7886430e4a35c1eb65599
Jens Bäckvall <jensbackvall@t-nova.org>
2017-03-20 14:18:54 +0100

    Create Fancy_Hello.java

---
+++ Java Exercises/1st term/week 6/Fancy_Hello.java
@@ -0,0 +1,13 @@
+public class Fancy_Hello {
+      public static void main(String[] args) {
+         hello();
+         hello();
+         hello();
+      }
+      public static void hello() {
+         System.out.println("///*************\\\\\\");
+         System.out.println("|||Hello Dat17v2|||");
+         System.out.println("\\\\\\*************///");
+         System.out.println();
+      } 
+}
