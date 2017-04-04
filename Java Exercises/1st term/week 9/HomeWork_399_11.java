b26a5af66b843d09c3267fa75347e309c17da288
Jens Bäckvall <jensbackvall@t-nova.org>
2017-04-04 10:53:59 +0200

    update files

--- Java Exercises/1st term/week 9/HomeWork_399_11.java
+++ Java Exercises/1st term/week 9/HomeWork_399_11.java
@@ -1,18 +1,26 @@
 import java.util.*;
+import java.math.*;
 
 public class HomeWork_399_11 {
 
     public static void main(String[] args) {
 
-        Random r = new Random();
+        //Random r = new Random();
 
-        int num = r.nextInt(1000);
-
+        //int num = r.nextInt(1000);
+       /* 
         do {
             System.out.println("Random number: " + num);
             num = r.nextInt(1000);
         } while (num < 900);
-        System.out.println("Random number: " + num);
+        System.out.println("Random number: " + num); */
+        
+        double sum = 0;
+        double num2 = 0;
+        for (int i = 0; i < 64; i++) {
+           num2 = Math.pow(2, i);
+           sum = sum + num2;
+        }
+        System.out.println(sum);
     }
-
 }
