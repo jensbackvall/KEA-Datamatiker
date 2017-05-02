57b5af3922ed4c007560376547d57d703d9868be
Jens Bäckvall <jensbackvall@t-nova.org>
2017-05-02 11:41:16 +0200

    update TV

--- Java Exercises/1st term/week 18/TV.java
+++ Java Exercises/1st term/week 18/TV.java
@@ -10,6 +10,7 @@ public class TV {
 
     public void turnOn() {
         on = true;
+        printCurrentState();
     }
 
     public void turnOff() {
@@ -20,6 +21,7 @@ public class TV {
         on = true;
         if (newChannel > 0 && newChannel < 121) {
             channel = newChannel;
+            printCurrentState();
         } else {
             System.out.println("Kanalen skal være fra 1 t.o.m. 120");
             printCurrentState();
@@ -30,8 +32,10 @@ public class TV {
         if (on == true) {
             if (newVolumeLevel < 8) {
                 volumeLevel = newVolumeLevel;
+                printCurrentState();
             } else {
                 volumeLevel = 7;
+                printCurrentState();
             }
         }
     }
@@ -40,11 +44,14 @@ public class TV {
         if (on == false) {
             on = true;
             channel = 1;
+            printCurrentState();
         } else {
             if (channel < 120) {
                 channel++;
+                printCurrentState();
             } else {
                 channel = 1;
+                printCurrentState();
             }
         }
     }
@@ -53,24 +60,30 @@ public class TV {
         if (on == false) {
             on = true;
             channel = 120;
+            printCurrentState();
         } else {
             channel--;
+            printCurrentState();
         }
     }
 
     public void volumeUp() {
         if (on == true && volumeLevel < 7) {
                 volumeLevel++;
+                printCurrentState();
         }
     }
 
     public void volumeDown() {
         if (on == true && volumeLevel > 0) {
                 volumeLevel--;
+                printCurrentState();
         }
     }
 
     public void printCurrentState() {
+        System.out.println();
+        System.out.println("* input er modtaget * ");
         if (on == true) {
             System.out.println("Kanalen er: " + channel);
             System.out.println("Volumeniveauen er: " + volumeLevel);
--- Java Exercises/1st term/week 18/TestTV.java
+++ Java Exercises/1st term/week 18/TestTV.java
@@ -3,16 +3,12 @@ public class TestTV {
 
     public static void main(String[] args) {
         TV lg = new TV();
-    lg.setChannel(5);
-    lg.printCurrentState();
-    lg.channelUp();
-    lg.printCurrentState();
-    lg.volumeUp();
-    lg.printCurrentState();
-    lg.setChannel(130);
-    lg.setChannel(119);
-    lg.printCurrentState();
-    lg.turnOff();
-    lg.printCurrentState();
+        lg.setChannel(5);
+        lg.channelUp();
+        lg.volumeUp();
+        lg.setChannel(130);
+        lg.setChannel(119);
+        lg.turnOff();
+        lg.printCurrentState();
     }
 }
